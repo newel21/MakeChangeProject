ed9f78687f7c7432636b2a945bf5b874b4bb3865
newel <newel_21@yahoo.com>
2018-08-31 23:00:32 -0600

    Update: MakeChangeAppProject

--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -1,9 +1,42 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the price of the item: ");
+		double itemPrice = sc.nextDouble();
+		
+		System.out.print("Enter the amount of money: ");
+		double money = sc.nextDouble();
+	
 
+		if (money < itemPrice) {
+			System.out.println("That's not enough.");
+		}else if (money == itemPrice) {
+			System.out.println("Thank you");
+		}else if (money > itemPrice) {	
+			double change = money - itemPrice; 
+			System.out.println(change);
+		}
+		
+		
+		
+		sc.close();
+		
+		
+		
+		
+		
+
+		
 	}
-
+	
+		
 }
+
+
+
